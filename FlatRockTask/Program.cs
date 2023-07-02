bf37cdabf518fa91338370e3652cf5a47d85b63a
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2023-07-02 23:43:18 +0300

    create products

--- FlatRockTask/Program.cs
+++ FlatRockTask/Program.cs
@@ -1,4 +1,6 @@
-﻿using HtmlAgilityPack;
+﻿using FlatRockTask;
+using HtmlAgilityPack;
+using System.Globalization;
 
 HtmlDocument data = new HtmlDocument();
 data.Load("data.html");
@@ -12,5 +14,26 @@ var productPrices = data.DocumentNode.SelectNodes("//div[@class='pricing']/p/spa
  .Select(x => x.InnerText).ToList();
 
 var productRatings = data.DocumentNode.SelectNodes("/div[@class='item']")
-    .Select(x => x.GetAttributeValue("rating", ""))
+    .Select(x => x.GetAttributeValue("rating", "")).Select(x => decimal.Parse(x, CultureInfo.InvariantCulture))
     .ToList();
+
+//List<decimal> ratings = productRatings.Select(x => decimal.Parse(x, CultureInfo.InvariantCulture)).ToList();
+List<decimal> normalRatings = new List<decimal>();
+
+foreach (var item in productRatings)
+{
+    if (item > 10.00M)
+    {
+        normalRatings.Add((item / 100) * 5.0M);
+    }
+    else if (item > 5.00m)
+    {
+        normalRatings.Add((item / 10) * 5.0M);
+    }
+    else
+    {
+        normalRatings.Add(item);
+    }
+}
+
+var product = new List<Product>();
