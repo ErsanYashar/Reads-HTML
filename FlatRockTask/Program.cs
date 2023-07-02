27af9aa04bfffc453aa76d9acd537f033d70c2da
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2023-07-02 23:26:14 +0300

    extraction of non-passable data

---
+++ FlatRockTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+
+HtmlDocument data = new HtmlDocument();
+data.Load("data.html");
+
+var productNames = data.DocumentNode.SelectNodes("//div[@class='item']//figure//a/img")
+  .Select(x => x.GetAttributeValue("alt", "")).ToList();
+
+var names = productNames.Select(x => HtmlAgilityPack.HtmlEntity.DeEntitize(x));
+
+var productPrices = data.DocumentNode.SelectNodes("//div[@class='pricing']/p/span")
+ .Select(x => x.InnerText).ToList();
+
+var productRatings = data.DocumentNode.SelectNodes("/div[@class='item']")
+    .Select(x => x.GetAttributeValue("rating", ""))
+    .ToList();
