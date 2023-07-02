d171e17ba3aeb50c0c3f225d4b0d0b0e3929f51b
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2023-07-02 23:04:08 +0300

    create class Product

---
+++ FlatRockTask/FlatRockTask/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace FlatRockTask
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
