e698136f1281fcf907e00f6dbf6dc395d0f3365d
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2023-07-09 01:03:49 +0300

    arrangement

--- FlatRockTask/Program.cs
+++ FlatRockTask/Program.cs
@@ -7,12 +7,13 @@ HtmlDocument data = new HtmlDocument();
 data.Load("data.html");
 
 var productNames = data.DocumentNode.SelectNodes("//div[@class='item']//figure//a/img")
-  .Select(x => x.GetAttributeValue("alt", "")).ToList();
-
-var names = productNames.Select(x => HtmlAgilityPack.HtmlEntity.DeEntitize(x));
+    .Select(x => x.GetAttributeValue("alt", ""))
+    .Select(x => HtmlAgilityPack.HtmlEntity.DeEntitize(x))
+    .ToList();
 
 var productPrices = data.DocumentNode.SelectNodes("//div[@class='pricing']/p/span")
- .Select(x => x.InnerText).ToList();
+    .Select(x => x.InnerText)
+    .ToList();
 
 var productRatings = data.DocumentNode.SelectNodes("/div[@class='item']")
     .Select(x => x.GetAttributeValue("rating", ""))
@@ -24,11 +25,8 @@ List<Product> products = new List<Product>();
 
 foreach (var item in productRatings)
 {
-    if (item > 10.00M)
-    {
-        normalRatings.Add((item / 100) * 5.0M);
-    }
-    else if (item > 5.00m)
+
+    if (item > 5.00M)
     {
         normalRatings.Add((item / 10) * 5.0M);
     }
@@ -40,13 +38,13 @@ foreach (var item in productRatings)
 
 for (int i = 0; i < productNames.Count; i++)
 {
-    var price = productPrices.ElementAt(i).Split("$", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    var price = productPrices[i].Split("$", StringSplitOptions.RemoveEmptyEntries).ToArray();
 
     products.Add(new Product
     {
-        ProductName = names.ElementAt(i),
+        ProductName = productNames[i],
         Price = price[1].Replace(",", ""),
-        Rating = Math.Round(normalRatings.ElementAt(i), 1)
+        Rating = Math.Round(normalRatings[i], 1)
     });
 }
 
